9a2429343e3b2a74d9b55bc7b5dcd8f093f83814
Cristian-Marcano <cristianmm.1112@gmail.com>
2026-08-14 10:39:40 -0400

    fix: <Image /> element astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService()
+  },
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
