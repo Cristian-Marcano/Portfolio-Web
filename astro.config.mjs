d5e7eb61f1bc5181ee8e7055a89b5006a8b569ae
Cristian-Marcano <cristianmm.1112@gmail.com>
2026-08-14 11:04:39 -0400

    hotfix: image path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: passthroughImageService()
-  },
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
