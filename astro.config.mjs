9d0ffbe46c988e84fbe3991bde44bb1e73babe47
Cristian-Marcano <cristianmm.1112@gmail.com>
2026-08-14 15:57:10 -0400

    feat: configure site URL in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://cristian-marcano.com",
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
     routing: {
-      prefixDefaultLocale: false, // "/" → español, "/en/" → inglés
+      prefixDefaultLocale: false,
     },
   },
 });
